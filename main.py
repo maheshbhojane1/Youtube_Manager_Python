c3b3bd170f82d8bd2bdbf53f8226a6ee088bc966
Mahesh Bhojane <maheshbhojane200@gmail.com>
2026-04-07 11:50:31 +0530

    update the functions

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import json
 
 
+
 def load_Data():
     try: 
         with open("youtube.txt", "r") as file:
-            return json.load(file)
+            content = file.read().strip()
+            if not content:  # if file is empty
+                return []
+            return json.loads(content)
     except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
         return []
 
 def save_Data(videos):
     with open("youtube.txt", "w") as file:
-        json.dump(videos, file, indent=4)
+        json.dump(videos, file)
 
 def list_All_Videos(videos):
-   for video, index in enumerate(videos, start=1):
-       print(f"{index} .")
-
+    print("\n")
+    print("*" * 70)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. Name: {video['name']}, Time: {video['time']}")
+    print("\n")
+    print("*" * 70)
 
 def add_videos(videos):
     
@@ -30,12 +39,25 @@ def add_videos(videos):
     save_Data(videos)
 
 def update(videos):
-    pass
+    list_All_Videos(videos)
+
+    index = int(input("Enter the number for update the video: "))
+    if 1 <= index <= len(videos):
+        name = input("Enter the title: ")
+        time = input("Enter the time: ")
+
+        videos[index-1] = {'name': name, 'time': time}
+        save_Data(videos)
+
+    
 
 def delete_video(videos):
-    pass
+    list_All_Videos(videos)
+    index = int(input("Enter the numbe for delete: "))
+    if 1 <= index <= len(videos):
+        del(videos[index -1])
 
-
+        save_Data(videos)
 
 
 def main():
